15f1c0c22d306d98d1137d753262dc7cc45f6f84
Yuki Nishimura <yukinkopen@gmail.com>
2022-02-14 11:00:50 +0900

    MainActivity でカメラの権限を付与する形に変更

--- app/src/main/java/com/example/mlkitsample/MainActivity.kt
+++ app/src/main/java/com/example/mlkitsample/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.mlkitsample
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.mlkit.md.LiveBarcodeScanningActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val RequestCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            requestCameraPermission()
+            return
+        }
+        goToBarcodeActivity()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            RequestCode -> {
+                if (isPermissionGranted(grantResults)) {
+                    goToBarcodeActivity()
+                } else {
+                    Toast.makeText(this, "パーミッションが付与されませんでした", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun isPermissionGranted(grantResults: IntArray): Boolean {
+        return grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun goToBarcodeActivity() {
+        val intent = Intent(this, LiveBarcodeScanningActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), RequestCode)
     }
 }
