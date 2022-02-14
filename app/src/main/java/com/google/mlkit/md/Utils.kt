70455b28de629f7a4deefce5b0a042673fa0e270
Yuki Nishimura <yukinkopen@gmail.com>
2022-02-14 17:40:01 +0900

    利用していないコードを削除

--- app/src/main/java/com/google/mlkit/md/Utils.kt
+++ app/src/main/java/com/google/mlkit/md/Utils.kt
@@ -16,35 +16,16 @@
 
 package com.google.mlkit.md
 
-import android.app.Activity
-import android.content.ContentResolver
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.content.res.Configuration
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Matrix
-import android.graphics.Paint
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffXfermode
-import android.graphics.Rect
-import android.graphics.RectF
-import android.graphics.YuvImage
+import android.graphics.*
 import android.hardware.Camera
-import android.net.Uri
 import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat.checkSelfPermission
-import androidx.exifinterface.media.ExifInterface
 import com.google.mlkit.md.camera.CameraSizePair
 import com.google.mlkit.vision.common.InputImage
 import java.io.ByteArrayOutputStream
-import java.io.IOException
-import java.io.InputStream
 import java.nio.ByteBuffer
-import java.util.ArrayList
+import java.util.*
 import kotlin.math.abs
 
 /** Utility class to provide helper methods.  */
@@ -59,34 +40,6 @@ object Utils {
     internal const val REQUEST_CODE_PHOTO_LIBRARY = 1
 
     private const val TAG = "Utils"
-
-    internal fun requestRuntimePermissions(activity: Activity) {
-
-        val allNeededPermissions = getRequiredPermissions(activity).filter {
-            checkSelfPermission(activity, it) != PackageManager.PERMISSION_GRANTED
-        }
-
-        if (allNeededPermissions.isNotEmpty()) {
-            ActivityCompat.requestPermissions(
-                activity, allNeededPermissions.toTypedArray(), /* requestCode= */ 0
-            )
-        }
-    }
-
-    internal fun allPermissionsGranted(context: Context): Boolean = getRequiredPermissions(
-        context
-    )
-        .all { checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED }
-
-    private fun getRequiredPermissions(context: Context): Array<String> {
-        return try {
-            val info = context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_PERMISSIONS)
-            val ps = info.requestedPermissions
-            if (ps != null && ps.isNotEmpty()) ps else arrayOf()
-        } catch (e: Exception) {
-            arrayOf()
-        }
-    }
 
     fun isPortraitMode(context: Context): Boolean =
         context.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
@@ -135,18 +88,6 @@ object Utils {
         return validPreviewSizes
     }
 
-    fun getCornerRoundedBitmap(srcBitmap: Bitmap, cornerRadius: Int): Bitmap {
-        val dstBitmap = Bitmap.createBitmap(srcBitmap.width, srcBitmap.height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(dstBitmap)
-        val paint = Paint()
-        paint.isAntiAlias = true
-        val rectF = RectF(0f, 0f, srcBitmap.width.toFloat(), srcBitmap.height.toFloat())
-        canvas.drawRoundRect(rectF, cornerRadius.toFloat(), cornerRadius.toFloat(), paint)
-        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
-        canvas.drawBitmap(srcBitmap, 0f, 0f, paint)
-        return dstBitmap
-    }
-
     /** Convert NV21 format byte buffer to bitmap. */
     fun convertToBitmap(data: ByteBuffer, width: Int, height: Int, rotationDegrees: Int): Bitmap? {
         data.rewind()
@@ -169,85 +110,5 @@ object Utils {
             Log.e(TAG, "Error: " + e.message)
         }
         return null
-    }
-
-    internal fun openImagePicker(activity: Activity) {
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.addCategory(Intent.CATEGORY_OPENABLE)
-        intent.type = "image/*"
-        activity.startActivityForResult(intent, REQUEST_CODE_PHOTO_LIBRARY)
-    }
-
-    @Throws(IOException::class)
-    internal fun loadImage(context: Context, imageUri: Uri, maxImageDimension: Int): Bitmap? {
-        var inputStreamForSize: InputStream? = null
-        var inputStreamForImage: InputStream? = null
-        try {
-            inputStreamForSize = context.contentResolver.openInputStream(imageUri)
-            var opts = BitmapFactory.Options()
-            opts.inJustDecodeBounds = true
-            BitmapFactory.decodeStream(inputStreamForSize, null, opts)/* outPadding= */
-            val inSampleSize = Math.max(opts.outWidth / maxImageDimension, opts.outHeight / maxImageDimension)
-
-            opts = BitmapFactory.Options()
-            opts.inSampleSize = inSampleSize
-            inputStreamForImage = context.contentResolver.openInputStream(imageUri)
-            val decodedBitmap = BitmapFactory.decodeStream(inputStreamForImage, null, opts)/* outPadding= */
-            return maybeTransformBitmap(
-                context.contentResolver,
-                imageUri,
-                decodedBitmap
-            )
-        } finally {
-            inputStreamForSize?.close()
-            inputStreamForImage?.close()
-        }
-    }
-
-    private fun maybeTransformBitmap(resolver: ContentResolver, uri: Uri, bitmap: Bitmap?): Bitmap? {
-        val matrix: Matrix? = when (getExifOrientationTag(resolver, uri)) {
-            ExifInterface.ORIENTATION_UNDEFINED, ExifInterface.ORIENTATION_NORMAL ->
-                // Set the matrix to be null to skip the image transform.
-                null
-            ExifInterface.ORIENTATION_FLIP_HORIZONTAL -> Matrix().apply { postScale(-1.0f, 1.0f) }
-
-            ExifInterface.ORIENTATION_ROTATE_90 -> Matrix().apply { postRotate(90f) }
-            ExifInterface.ORIENTATION_TRANSPOSE -> Matrix().apply { postScale(-1.0f, 1.0f) }
-            ExifInterface.ORIENTATION_ROTATE_180 -> Matrix().apply { postRotate(180.0f) }
-            ExifInterface.ORIENTATION_FLIP_VERTICAL -> Matrix().apply { postScale(1.0f, -1.0f) }
-            ExifInterface.ORIENTATION_ROTATE_270 -> Matrix().apply { postRotate(-90.0f) }
-            ExifInterface.ORIENTATION_TRANSVERSE -> Matrix().apply {
-                postRotate(-90.0f)
-                postScale(-1.0f, 1.0f)
-            }
-            else ->
-                // Set the matrix to be null to skip the image transform.
-                null
-        }
-
-        return if (matrix != null) {
-            Bitmap.createBitmap(bitmap!!, 0, 0, bitmap.width, bitmap.height, matrix, true)
-        } else {
-            bitmap
-        }
-    }
-
-    private fun getExifOrientationTag(resolver: ContentResolver, imageUri: Uri): Int {
-        if (ContentResolver.SCHEME_CONTENT != imageUri.scheme && ContentResolver.SCHEME_FILE != imageUri.scheme) {
-            return 0
-        }
-
-        var exif: ExifInterface? = null
-        try {
-            resolver.openInputStream(imageUri)?.use { inputStream -> exif = ExifInterface(inputStream) }
-        } catch (e: IOException) {
-            Log.e(TAG, "Failed to open file to read rotation meta data: $imageUri", e)
-        }
-
-        return if (exif != null) {
-            exif!!.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
-        } else {
-            ExifInterface.ORIENTATION_UNDEFINED
-        }
     }
 }
